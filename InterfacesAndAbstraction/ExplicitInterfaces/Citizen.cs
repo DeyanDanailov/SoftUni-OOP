59aa61740317f8c6fc825f9324ca269af9cdcf0e
DeyanDanailov <stormbringer_ss@abv.bg>
2020-11-06 23:48:06 +0200

    explicit interfaces problem done

---
+++ InterfacesAndAbstraction/ExplicitInterfaces/Citizen.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace ExplicitInterfaces
+{
+    public class Citizen : IPerson, IResident
+    {
+        
+
+        public Citizen(string name)
+        {
+            this.Name = name;
+            
+        }
+
+        public string Name { get; private set; }
+
+        public int Age { get; set; }
+
+        string IResident.Name { get; }
+
+        string IResident.Country { get; set; }
+        
+
+        public string GetName()
+        {
+            return this.Name;
+        }
+
+        string IResident.GetName()
+        {
+            return $"Mr/Ms/Mrs {this.Name}";
+        }
+    }
+}
---
+++ InterfacesAndAbstraction/ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExplicitInterfaces
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                var cmdArgs = command.Split();
+                var name = cmdArgs[0];
+                var country = cmdArgs[1];
+                var age = int.Parse(cmdArgs[2]);
+                var human = new Citizen(name);
+                human.Age = age;
+                ((IResident)human).Country = country;
+                Console.WriteLine(human.GetName());
+                Console.WriteLine(((IResident)human).GetName());
+            }
+        }
+    }
+}
