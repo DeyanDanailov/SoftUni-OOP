4b898a293fdfa2ebe2deb5dc903caa5a75e61adc
DeyanDanailov <stormbringer_ss@abv.bg>
2020-11-08 00:21:05 +0200

    shapes problem done

---
+++ Polymorphism/Shapes/Circle.cs
@@ -0,0 +1,62 @@
+﻿
+
+using System;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+        public double Radius {
+            get
+            {
+                return this.radius;
+            }
+            private set
+            {
+                this.radius = value;
+            }
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * this.Radius * this.Radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+          
+            return 2 * Math.PI * this.Radius;
+        }
+        public override string Draw()
+        {
+            //var sb = new StringBuilder();
+            //double rIn = this.radius - 0.4;
+            //double rOut = this.radius + 0.4;
+            //for (double y = this.radius; y >= -this.radius; --y)
+            //{
+            //    for (double x = -this.radius; x < rOut; x += 0.5)
+            //    {
+            //        double value = x * x + y * y;
+            //        if (value >= rIn * rIn && value < rOut * rOut)
+            //        {
+            //            sb.Append("*");
+            //        }
+            //        else
+            //        {
+            //            sb.Append(" ");
+            //        }
+            //    }
+            //    sb.AppendLine();
+            //}
+            //return sb.ToString().TrimEnd();
+            return base.Draw() + this.GetType().Name;
+        }
+        
+    }
+}
---
+++ Polymorphism/Shapes/Rectangle.cs
@@ -0,0 +1,81 @@
+﻿
+
+using System;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(double height, double width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            private set
+            {
+
+
+                this.height = value;
+
+            }
+        }
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            private set
+            {
+
+                this.width = value;
+
+            }
+        }
+
+        public override double CalculateArea()
+        {
+            return this.Height * this.Width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * (this.Height + this.Width);
+        }
+        public override string Draw()
+        {
+            //var sb = new StringBuilder();
+            //sb.AppendLine(Drawline(this.width, '*', '*'));
+            //for (int i = 1; i < this.height - 1; ++i)
+            //{
+            //    sb.AppendLine(Drawline(this.width, '*', ' '));
+            //}
+            //sb.AppendLine(Drawline(this.width, '*', '*'));
+            //return sb.ToString().TrimEnd();
+            return base.Draw() + this.GetType().Name;
+        }
+
+        //private string Drawline(double width, char end, char mid)
+        //{
+        //    var sb = new StringBuilder();
+        //    sb.Append(end);
+        //    for (int i = 1; i < width - 1; ++i)
+        //    {
+        //        sb.Append(mid);
+        //    }
+        //    sb.AppendLine((end).ToString());
+        //    return sb.ToString().TrimEnd();
+        //}
+    }
+}
