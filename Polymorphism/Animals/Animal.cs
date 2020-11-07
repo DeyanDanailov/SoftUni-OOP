75bd6e80b1a32392ce6aafe3dbd4c7901f8bcfc0
DeyanDanailov <stormbringer_ss@abv.bg>
2020-11-07 19:07:40 +0200

    polymorphism lab problems done

---
+++ Polymorphism/Animals/Animal.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private string favouriteFood;
+        public Animal(string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavoutireFood = favouriteFood;
+        }
+        public string Name { get; protected set; }
+        public string FavoutireFood { get; protected set; }
+        public virtual string ExplainSelf()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"I am {this.Name} and my fovourite food is {this.FavoutireFood}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
