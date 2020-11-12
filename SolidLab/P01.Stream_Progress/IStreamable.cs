01f898af1630c326397e8606f1728d2594f87a83
DeyanDanailov <stormbringer_ss@abv.bg>
2020-11-12 18:59:53 +0200

    solid lab problems

---
+++ SolidLab/P01.Stream_Progress/IStreamable.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace P01.Stream_Progress
+{
+     public interface IStreamable
+    {
+        public int Length { get; }
+
+        public int BytesSent { get; }
+    }
+}
---
+++ SolidLab/P01.Stream_Progress/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace P01.Stream_Progress
+{
+    public class Program
+    {
+        static void Main()
+        {
+            IStreamable myFile = new Music("Dido", "Dido e pich", 4, 1);
+            StreamProgressInfo myStream = new StreamProgressInfo(myFile);
+            var percent = myStream.CalculateCurrentPercent();
+            Console.WriteLine(percent);
+        }
+    }
+}
