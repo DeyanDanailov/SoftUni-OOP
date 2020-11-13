4a899e1b3b2218d1b562636e7fcfa1dd21f35df6
DeyanDanailov <stormbringer_ss@abv.bg>
2020-11-13 19:08:54 +0200

    up

---
+++ SolidExercise/Logger/Models/Logger.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Collections.Generic;
+using Logger.Models.Contracts;
+
+namespace Logger.Models
+{
+    public class Logger : ILogger
+    {
+        private ICollection<IAppender> appenders;
+        public Logger(ICollection <IAppender> appenders)
+        {
+            this.appenders = appenders;
+        }
+        public IReadOnlyCollection<IAppender> Appenders => (IReadOnlyCollection<IAppender>)this.appenders;
+
+        public void Log(IError error)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
