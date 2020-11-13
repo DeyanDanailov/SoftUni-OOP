96a1f7f7ff7ff886d72cb2ce25a76020b559e318
DeyanDanailov <stormbringer_ss@abv.bg>
2020-11-13 16:45:08 +0200

    up

---
+++ SolidExercise/Logger/Models/Appenders/ConsoleAppender.cs
@@ -0,0 +1,37 @@
+﻿
+
+using System;
+using System.Globalization;
+using Logger.Models.Contracts;
+using Logger.Models.Enumerations;
+
+namespace Logger.Models.Appenders
+{
+    public class ConsoleAppender : IAppender
+    {
+        public ConsoleAppender(ILayout layout, Level level)
+        {
+            this.Layout = layout;
+            this.Level = level;
+        }
+        public ILayout Layout {get; private set;}
+
+        public Level Level { get; private set; }
+
+        public void Append(IError error)
+        {
+            string format = this.Layout.Format;
+            DateTime dateTime = error.DateTime;
+            string message = error.Message;
+            Level level = error.Level;
+
+            string formattedMessage = String.Format(format, dateTime.ToString("M/dd/yyyy H:mm:ss tt", CultureInfo.InvariantCulture), message, level.ToString());
+
+            Console.WriteLine(formattedMessage);
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
