752bf1fd6cd54f499f861b101652a80a904ae6e1
DeyanDanailov <stormbringer_ss@abv.bg>
2020-11-17 12:10:05 +0200

    reflection lab problems done

--- Reflection/Stealer/Spy.cs
+++ Reflection/Stealer/Spy.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 
@@ -19,6 +20,42 @@ namespace Stealer
                 var field = type.GetField(fieldToSpy, BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public);
                 string spiedValue = field.GetValue(test).ToString();
                 sb.AppendLine($"{field.Name} = {spiedValue}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+        public string AnalyzeAcessModifiers(string className)
+        {
+            var sb = new StringBuilder();
+            var type = Type.GetType(className);
+            var publicFields = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static);
+            foreach (var field in publicFields)
+            {
+                sb.AppendLine($"{field.Name} must be private!");
+            }
+            var nonpublicMethods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (var method in nonpublicMethods.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} have to be public!");
+            }
+            var publicMethods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+            foreach (var method in publicMethods.Where(m=>m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} have to be private!");
+            }
+            
+            return sb.ToString().TrimEnd();
+        }
+        public string RevealPrivateMethods(string className)
+        {
+            var sb = new StringBuilder();
+            var type = Type.GetType(className);
+            MethodInfo[] privateMethods = type.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+            sb.AppendLine($"All Private Methods of Class: {type.FullName}");
+            sb.AppendLine($"Base Class: {type.BaseType.Name}");
+            
+            foreach (var method in privateMethods)
+            {               
+                sb.AppendLine(method.Name);
             }
             return sb.ToString().TrimEnd();
         }
