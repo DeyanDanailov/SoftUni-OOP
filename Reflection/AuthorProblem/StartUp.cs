a809fe1feb28d4820f70e6c7fb66e8092314f69f
DeyanDanailov <stormbringer_ss@abv.bg>
2020-11-17 14:05:41 +0200

    up

---
+++ Reflection/AuthorProblem/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Ventsi")]
+    public class StartUp
+    {
+        [Author("Gosho")]
+        static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+        [Author("Dido")]
+        private static void Print()
+        {
+            Console.WriteLine("Dido e pich");
+        }
+    }
+}
