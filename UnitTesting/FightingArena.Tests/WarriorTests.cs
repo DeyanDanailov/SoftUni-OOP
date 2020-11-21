e963536e370d9e084ae4bc12de7f85ac54f52217
DeyanDanailov <stormbringer_ss@abv.bg>
2020-11-21 17:17:30 +0200

    up

--- UnitTesting/FightingArena.Tests/WarriorTests.cs
+++ UnitTesting/FightingArena.Tests/WarriorTests.cs
@@ -1,18 +1,133 @@
+using System;
+using FightingArena;
 using NUnit.Framework;
 
 namespace Tests
 {
     public class WarriorTests
     {
+        private const string INVALID_NAME = "Name should not be empty or whitespace!";
+        private const string INVALID_DAMAGE = "Damage value should be positive!";
+        private const string INVALID_HP = "HP should not be negative!";
+        private const string TOO_LOW_HP = "Your HP is too low in order to attack other warriors!";
+        private const string TOO_STRONG_ENEMY = "You are trying to attack too strong enemy";
+        private const int MIN_ATTACK_HP = 30;
+        private Warrior warrior;
+        private Warrior secondWarrior;
         [SetUp]
         public void Setup()
         {
+            this.warrior = new Warrior("Sudjuka", 50, 18);
+            this.secondWarrior = new Warrior("Bastuna", 78, 50);
         }
 
         [Test]
-        public void Test1()
+        public void CheckNameGetter()
         {
-            Assert.Pass();
+            var expected = "Sudjuka";
+            var actual = this.warrior.Name;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("", 40, 80)]
+        [TestCase(null, 40, 80)]
+        public void NameShoulThrowExcIfNullOrWhiteSpace(string name, int damage, int hp)
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior(name, damage, hp), //Act
+                INVALID_NAME);
+        }
+
+        [Test]
+        public void CheckDamageGetter()
+        {
+            var expected = 50;
+            var actual = this.warrior.Damage;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("Mamula", 0, 80)]
+        [TestCase("Mamula", -890, 80)]
+        public void DamageShoulThrowExcIfNullOrNegative(string name, int damage, int hp)
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior(name, damage, hp), //Act
+                INVALID_DAMAGE);
+        }
+
+        [Test]
+        public void CheckHPGetter()
+        {
+            var expected = 18;
+            var actual = this.warrior.HP;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("Mamula", 78, -5)]
+        public void HPShoulThrowExcIfNegative(string name, int damage, int hp)
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior(name, damage, hp), //Act
+                INVALID_HP);
+        }
+
+        [TestCase("Kochana", 78, 26)]
+        public void AttackShouldThrowExcIfHPLessThanMinAttacker(string name, int damage, int hp)
+        {
+            //Arrange
+            var warriorToAttack = new Warrior(name, damage, hp);
+
+            //Assert
+            Assert.Throws<InvalidOperationException>(() => 
+            this.warrior.Attack(warriorToAttack), TOO_LOW_HP); //Act
+
+            
+        }
+        [TestCase("Kochana", 78, 26)]
+        public void AttackShouldThrowExcIfHPLessThanMinAttacked(string name, int damage, int hp)
+        {
+            //Arrange
+            var warriorToAttack = new Warrior(name, damage, hp);
+
+            //Assert
+            Assert.Throws<InvalidOperationException>(() =>
+            this.secondWarrior.Attack(warriorToAttack), $"Enemy HP must be greater than {MIN_ATTACK_HP} in order to attack him!"); //Act            
+        }
+
+        [TestCase("Kochana", 78, 45)]
+        public void AttackShouldThrowExcIfHPLessThanDamageOfAttacked(string name, int damage, int hp)
+        {
+            //Arrange
+            var warriorToAttack = new Warrior(name, damage, hp);
+
+            //Assert
+            Assert.Throws<InvalidOperationException>(() =>
+            this.secondWarrior.Attack(warriorToAttack), TOO_STRONG_ENEMY); //Act            
+        }
+
+        [TestCase("Kochana", 39, 45)]
+        public void AttackShouldDecreaseAtackerHP(string name, int damage, int hp)
+        {
+            //Arrange
+            var warriorToAttack = new Warrior(name, damage, hp);
+            //Act
+            this.secondWarrior.Attack(warriorToAttack);
+            var expected = 11;
+            var actual = this.secondWarrior.HP;
+
+            //AssertOne
+            Assert.AreEqual(expected, actual);
+            //AssertTwo
+            Assert.AreEqual(0, warriorToAttack.HP);
+        }
+
+        [TestCase("Kochana", 39, 79)]
+        public void AttackShouldDecreaseAtackedHPIfGreaterThanAttackerDamage(string name, int damage, int hp)
+        {
+            //Arrange
+            var warriorToAttack = new Warrior(name, damage, hp);
+            //Act
+            this.secondWarrior.Attack(warriorToAttack);
+                       
+            //AssertTwo
+            Assert.AreEqual(1, warriorToAttack.HP);
         }
     }
 }
