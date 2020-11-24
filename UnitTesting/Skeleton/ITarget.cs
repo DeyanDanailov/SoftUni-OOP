252566d222a375aed631e47efca9f9aac1112e8e
DeyanDanailov <stormbringer_ss@abv.bg>
2020-11-24 17:30:50 +0200

    up

---
+++ UnitTesting/Skeleton/ITarget.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Skeleton
+{
+    public interface ITarget
+    {
+        int Health { get; }
+        void TakeAttack(int attackPoints);
+        int GiveExperience();
+        bool IsDead();
+    }
+}
