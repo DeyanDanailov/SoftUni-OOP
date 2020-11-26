f6a31e7dff31124ef029352924cdc405ecda3c12
DeyanDanailov <stormbringer_ss@abv.bg>
2020-11-26 17:20:11 +0200

    up

--- ExamPreparation/C# OOP Retake Exam - 22 August 2020/UnitTests-Skeleton/TheRace.Tests/RaceEntryTests.cs
+++ ExamPreparation/C# OOP Retake Exam - 22 August 2020/UnitTests-Skeleton/TheRace.Tests/RaceEntryTests.cs
@@ -7,11 +7,14 @@ namespace TheRace.Tests
     public class RaceEntryTests
     {
         private RaceEntry raceEntry;
+        private UnitDriver captivaDriver;
+        private const int MIN_PARTICIPANTS_COUNT = 2;
 
         [SetUp]
         public void Setup()
         {
             raceEntry = new RaceEntry();
+            captivaDriver = new UnitDriver("Sudjuka", new UnitCar("Captiva", 190, 2000));
         }
 
         [Test]
@@ -38,15 +41,71 @@ namespace TheRace.Tests
                     this.raceEntry.AddDriver(nullDriver), "Driver cannot be null.");
 
         }
+        [Test]
         public void AddShouldThrowExcIfUnitDriverAddExistingDriver()
+        {           
+           //Arrange
+            this.raceEntry.AddDriver(this.captivaDriver);
+
+            //Act & Assert
+            Assert.Throws<InvalidOperationException>(() =>
+                    this.raceEntry.AddDriver(this.captivaDriver), "Driver {0} added in race.", this.captivaDriver.Name);
+
+        }
+
+        [Test]
+        public void AddShouldThrowExcIfUnitDriverAddDriverWithExistingName()
         {
             //Arrange
-            UnitDriver captivaDriver = new UnitDriver("Sudjuka", new UnitCar("Captiva", 185, 2000));
-            this.raceEntry.AddDriver(captivaDriver);
-            //Assert
-            Assert.Throws<InvalidOperationException>(() =>
-                    this.raceEntry.AddDriver(captivaDriver), "Driver {0} added in race.", captivaDriver.Name);
+            UnitDriver alfaDriver = new UnitDriver("Sudjuka", new UnitCar("Alfa", 140, 1900));
+            this.raceEntry.AddDriver(this.captivaDriver);
 
+            //Act & Assert
+            Assert.Throws<InvalidOperationException>(() =>
+                    this.raceEntry.AddDriver(this.captivaDriver), "Driver {0} added in race.", this.captivaDriver.Name);
+
+        }
+        [Test]
+        public void AddShouldIncreaseDriverCount()
+        {
+            //Arrange
+            var expectedCount = 1;
+
+            //Act
+            this.raceEntry.AddDriver(this.captivaDriver);
+            var actual = this.raceEntry.Counter;
+
+            //Assert
+            Assert.AreEqual(expectedCount, actual);
+
+        }
+
+        [Test]
+        public void CalculateAverageHorsePowerShouldThrowExcIfCounterLessThanMin()
+        {
+            //Arrange
+            this.raceEntry.AddDriver(this.captivaDriver);
+
+            //Act & Assert
+            Assert.Throws<InvalidOperationException>(() =>
+                   this.raceEntry.CalculateAverageHorsePower(),
+                   "The race cannot start with less than {0} participants.", MIN_PARTICIPANTS_COUNT);
+        }
+
+        [Test]
+        public void CalculateAverageHorsePowerShouldreturnCorrectAverageHorsePower()
+        {
+            //Arrange
+            UnitDriver alfaDriver = new UnitDriver("Giugiaro", new UnitCar("Alfa", 140, 1900));
+            this.raceEntry.AddDriver(this.captivaDriver);
+            this.raceEntry.AddDriver(alfaDriver);
+            var expected = 165;
+
+            //Act
+            var actual = this.raceEntry.CalculateAverageHorsePower();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
