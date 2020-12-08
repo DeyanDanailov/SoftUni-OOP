c8140bca7222071ce19dde144f1f26e033c62c04
DeyanDanailov <stormbringer_ss@abv.bg>
2020-12-08 19:53:53 +0200

    up

---
+++ ExamPreparation/C# OOP Basics Exam Retake - 19 April 2019/PlayersAndMonsters/Core/Factories/CardFactory.cs
@@ -0,0 +1,22 @@
+﻿
+
+using PlayersAndMonsters.Core.Factories.Contracts;
+using PlayersAndMonsters.Models.Cards.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace PlayersAndMonsters.Core.Factories
+{
+    public class CardFactory : ICardFactory
+    {
+        public ICard CreateCard(string type, string name)
+        {
+            Type cardType = Assembly
+               .GetExecutingAssembly()
+               .GetTypes().FirstOrDefault(t => t.Name == type);
+            ICard card = (ICard)Activator.CreateInstance(cardType, name);
+            return card;
+        }
+    }
+}
---
+++ ExamPreparation/C# OOP Basics Exam Retake - 19 April 2019/PlayersAndMonsters/Core/Factories/PlayerFactory.cs
@@ -0,0 +1,24 @@
+﻿
+
+using PlayersAndMonsters.Core.Factories.Contracts;
+using PlayersAndMonsters.Models.Players.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace PlayersAndMonsters.Core.Factories
+{
+    public class PlayerFactory : IPlayerFactory
+    {
+        public IPlayer CreatePlayer(string type, string username)
+        {
+            Type playerType = Assembly
+                .GetExecutingAssembly()
+                .GetTypes().FirstOrDefault(t => t.Name == type);
+
+            IPlayer player = (IPlayer)Activator.CreateInstance(playerType, username);
+
+            return player;
+        }
+    }
+}
